4e5fea9b52b8cc5de011c86bb3c689a0dc6da127
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-04-23 18:00:47 +0530

    shell

---
+++ 01-variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Ramesh :: hello Suresh how are u"
+echo "Suresh:: hi ramesh i am good "
+echo "Ramesh:: How is your DevOps learning Suresh"
+echo "Suresh:: It is going fine Ramesh"
