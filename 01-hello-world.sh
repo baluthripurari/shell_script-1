e2719f49b8324cf8ecc5fa643517c352a5869d14
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-04-23 17:45:50 +0530

    shell

---
+++ 01-hello-world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo"how to execute shell script"
+echo"sh <script name>"
+echo "./<script-name> ---->> this shold have execute permission?
+echo "by using the command chmod +x"
+echo "Hello, I am learning Shell-script"
