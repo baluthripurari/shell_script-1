be628b6b0965e3b55499ed694dfd02a25edc5aba
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-04-23 20:20:01 +0530

    shell script

---
+++ 04-varaible.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PERSON1=$1 #no space between = and value
+PERSON2=$2
+
+echo "$PERSON1 :: hello $PERSON2 how are u"
+echo "$PERSON2:: hi $PERSON1 i am good Thank you "
+echo "$PERSON1:: How is your DevOps learning $PERSON2"
+echo "$PERSON2:: It is going fine $PERSON1"
+echo "$PERSON1 :: $PERSON2"
+echo "$PERSON1 :: $PERSON2"
+
+
+#passing the arguments/paraments
+#command "sh variables.sh Balu Raju"
