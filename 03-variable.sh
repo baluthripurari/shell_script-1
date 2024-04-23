2c54d653b7eaab37c33913f225f64094edca1419
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-04-23 20:04:56 +0530

    shell script

---
+++ 03-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Balu #no space between = and value
+PERSON2=Raju
+
+echo "$PERSON1 :: hello $PERSON2 how are u"
+echo "$PERSON2:: hi $PERSON1 i am good Thank you "
+echo "$PERSON1:: How is your DevOps learning $PERSON2"
+echo "$PERSON2:: It is going fine $PERSON1"
+echo "$PERSON1 :: $PERSON2"
+echo "$PERSON1 :: $PERSON2"
