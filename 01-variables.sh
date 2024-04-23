1813e60b8875517b7571871e727e13648990fd1e
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-04-23 18:33:25 +0530

    shell script

--- 01-variables.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-echo "Balu :: hello Raju how are u"
---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "Balu :: hello Raju how are u"
+echo "Raju:: hi Balu i am good Thank you "
+echo "Balu:: How is your DevOps learning Raju"
+echo "Raju:: It is going fine Balu"
+echo "Balu :: Raju"
