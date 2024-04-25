6916aad5127d918e4f9ae158205f4b8995f6c0e4
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-04-25 17:03:06 +0530

    shell script

--- 08-sepcial-vars.sh
+++ 08-sepcial-vars.sh
@@ -9,5 +9,5 @@ echo "Home directory of current user: $HOME"
 echo "which user is running the script: $USER"
 echo "Host name: $HOSTNAME"
 echo "process ID of current shell script: $$"
-sleep 5 seconds
+sleep 5 &
 echo "process ID of last background command: $!"
