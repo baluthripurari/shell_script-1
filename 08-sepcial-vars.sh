26fb996d0aca805a5d764ec03afe242d4b759115
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-04-25 16:59:00 +0530

    shell script

---
+++ 08-sepcial-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "All variables: $@"
+echo "Number of Variables passwd: $#"
+echo "script name $0"
+echo "current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "which user is running the script: $USER"
+echo "Host name: $HOSTNAME"
+echo "process ID of current shell script: $$"
+echo "process ID of last background command: $!"
