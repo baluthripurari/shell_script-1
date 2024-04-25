d9989016489991dc97c0b09c9fd9f7e8e806814f
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-04-25 15:56:04 +0530

    shell script

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-echo "please enter username::"
+echo  "please enter username::"
 
-read  USERNAME #here username is variable
+read -s USERNAME #here username is variable
 
 echo "please enter password::"
 
-read  PASSWORD
+read -s PASSWORD
 
 echo "username is: $USERNAME, password is $PASSWORD"
