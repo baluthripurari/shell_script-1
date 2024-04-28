1c5b4b775541f8279bf09a27b5c0c2f0f31d024d
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-04-28 12:25:21 +0530

    shell

--- 10-installation.sh
+++ 10-installation.sh
@@ -6,7 +6,7 @@ if [$(id -u) -eq to 0]
 then
     "echo give $USERID is super user"
 else
-    echo given $USERID is not super user"
+    "echo given $USERID is not super user"
 fi
 
 dnf install mysql -y
