1e9f4568cf1ebeb70f97255d598ba51344240955
balu3014 <161106157+balu3014@users.noreply.github.com>
2024-04-28 12:31:19 +0530

    shell

--- 10-installation.sh
+++ 10-installation.sh
@@ -2,11 +2,13 @@
 
 
 USERID=$(id -u)
+
 if [$USERID -ne  0]
 then
     echo "please run this script with root access"
 else 
-    echo "ypu are super user"
+    echo "yo
+    u are super user"
 fi
 
 dnf install mysql -y
